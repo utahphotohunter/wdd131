faa4e657c4c5b4498c2707720e548ad8c34a9d03
utahphotohunter <mitch.hunter1197@gmail.com>
2024-10-22 20:43:25 -0600

    finished js for form

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,49 @@
+// product drop down menu 
+
+// list of products
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+// get id of drop down menu
+let selector = document.getElementById("product");
+
+// add products to drop down menu
+products.forEach(product => {
+    let newOption = document.createElement("option");
+    newOption.value = product.name;
+    selector.append(newOption)
+    newOption.textContent = product.name;
+});
+
+
+
+// footer inserts
+
 const footer = document.querySelector("#lastModified");
 const span = document.querySelector("#currentYear");
 let date = document.lastModified;
